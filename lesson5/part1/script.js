721c712373b863448cd43e31beb42d06d6d85da2
zerospirit79 <pavel.s.shilov@gmail.com>
2020-06-30 18:42:03 +0300

    3b update animation

--- lesson5/part1/script.js
+++ lesson5/part1/script.js
@@ -3,11 +3,16 @@ const closeBtn = document.querySelector('a');
 const showBtn = document.querySelector('button');
 
 closeBtn.addEventListener('click', function () {
-    windows.classList.add('hidden');
+    windows.classList.remove('animate__bounceInUp');
+    windows.classList.add('animate__backOutDown');
+    setTimeout(function () {
+        windows.classList.add('hidden');
+    }, 1000);
 });
 
 
 
 showBtn.addEventListener('click', function () {
-    windows.classList.remove('hidden');
+    windows.classList.remove('animate__backOutDown', 'hidden');
+    windows.classList.add('animate__animated', 'animate__bounceInUp');
 });
