40bb9e9581ee9907f5d9dd83f30c428b2e1b6270
zerospirit79 <pavel.s.shilov@gmail.com>
2020-07-01 09:07:07 +0300

    update part5 in lesson5

--- lesson5/part1/part2/script.js
+++ lesson5/part1/part2/script.js
@@ -4,5 +4,51 @@ buttons.forEach(function (button) {
         buttonClick(event);
     })
 });
+/**
+ * @param {MouseEvent} clickedButtonEvent
+ */
+function buttonClick(clickedButtonEvent) {
+    const cardText = clickedButtonEvent.target.parentNode;
 
-function buttonClick
+    const card = {
+        wrap: cardText,
+        img: cardText.querySelector('img'),
+        productName: cardText.querySelector('.productName'),
+        button: cardText.querySelector('button'),
+    };
+
+    const textOnButton = card.button.innerText;
+    if (textOnButton === 'Подробнее') {
+        showMoreText(card);
+    } else if (textOnButton === 'Отмена') {
+        hideMoreText(card);
+    }
+}
+
+/**
+ * @param {Object} card
+ * @param {HTMLDivElement} card.wrap
+ * @param {HTMLImageElement} card.img
+ * @param {HTMLDivElement} card.productName
+ * @param {HTMLButtonElement} card.button
+ */
+function hideMoreText(card) {
+    card.img.style.display = 'block';
+    card.wrap.querySelector('.desc').remove();
+    card.button.innerText = 'Подробнее';
+}
+
+/**
+ * @param {Object} card
+ * @param {HTMLDivElement} card.wrap
+ * @param {HTMLImageElement} card.img
+ * @param {HTMLDivElement} card.productName
+ * @param {HTMLButtonElement} card.button
+ */
+function showMoreText(card) {
+    card.img.style.display = 'none';
+    const text = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur vero ipsum cum ex earum ullam dolor minima obcaecati, officiis nulla harum facilis quis. Fugiat provident maiores, soluta distinctio aspernatur totam.';
+    card.productName.insertAdjacentHTML('afterend', `<div class="desc">${text}</div>`);
+    card.button.innerText = 'Отмена';
+
+}
